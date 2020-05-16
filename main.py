60cb6863d483f15cb177e59afdfb3c31d15e0d58
Joe <joe@joeth.uk>
2020-05-16 19:12:36 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, escape, request, render_template, send_from_directory
+from hashlib import sha256
+import youtube_dl
+
+app = Flask(__name__)
+
+def grab_video(url, videoformat):
+    # Set file name as a sha256 has of the URL
+    filename = sha256(url.encode()).hexdigest()
+    ydl_opts = {'verbose':True, 'outtmpl': ('content/'+filename)} # Set output template for Youtube-DL
+    
+    ## Check requested video format and appropriate options
+    if videoformat == "mp3":
+        ydl_opts.update({'postprocessors': [{'key': 'FFmpegExtractAudio'}], 'format': 'mp3'})
+    elif videoformat == "mp4":
+        ydl_opts.update({'format':'mp4'})
+    else:
+        return 400, "invalid format!"
+    
+    ## Download URL
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    return filename
+
+@app.route('/')
+def main_page():
+    return render_template('index.html')
+
+@app.route('/download', methods=['GET', 'POST'])
+def download_video():
+    return grab_video(request.form.get('url'), request.form.get('videoformat'))
+
+@app.route('/content/<path:path>')
+def serve_content(path):
+    return send_from_directory('content', path)
